e63540c82e04dde6094c8eb419279cfbf3bbaf71
JorgeLuix <tec.jorge.29@gmai.com>
2023-05-26 17:28:03 +0200

    add GameController

---
+++ app/Http/Controllers/GameController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Character;
+
+class GameController extends Controller
+{
+
+    public function index(){
+
+        $characters = Character::all();
+
+        return view ('home', compact('characters'));
+    }
+
+
+
+}
